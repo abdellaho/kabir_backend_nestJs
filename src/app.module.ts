817515f169122a0ed70d724f39d7fa94b38a86b6
abdellaho <bojaneabdellaho@gmail.com>
2025-09-24 09:37:59 +0100

    some changes in the work place

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductModule } from './modules/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'ka',
+      entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    }), 
     ProductModule,
   ],
   controllers: [AppController],
