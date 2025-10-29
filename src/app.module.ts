0f7693d5783856e9dd0c6d3df07e54ca4d082428
abdellaho <bojaneabdellaho@gmail.com>
2025-10-29 15:22:13 +0100

    delete param-prime-produit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { VilleModule } from './ville/ville.module';
 import { EtablissementModule } from './etablissement/etablissement.module';
 import { ParamPrimeModule } from './param-prime/param-prime.module';
 import { ParamComissModule } from './param-comiss/param-comiss.module';
-import { ParamPrimeProduitModule } from './param-prime-produit/param-prime-produit.module';
 import { RepertoireModule } from './repertoire/repertoire.module';
 import { StockModule } from './stock/stock.module';
 import { EmployeModule } from './employe/employe.module';
@@ -38,11 +37,10 @@ import { PlanComptableModule } from './plan-comptable/plan-comptable.module';
 import { SoldeModule } from './solde/solde.module';
 import { PersonnelModule } from './personnel/personnel.module';
 import { VoitureModule } from './voiture/voiture.module';
-import { FourniseurModule } from './fourniseur/fourniseur.module';
 import { FournisseurModule } from './fournisseur/fournisseur.module';
 
 @Module({
-  imports: [VilleModule, EtablissementModule, ParamPrimeModule, ParamComissModule, ParamPrimeProduitModule, RepertoireModule, StockModule, EmployeModule, DroitModule, DatabaseModule, AbsenceModule, AchatFactureModule, AchatLivraisonModule, BalanceModule, BilanModule, BonSortieModule, BulttinPaiModule, CaisseModule, ChequeModule, ComptaModule, ComptabiliteModule, CompteCaisseModule, DetAchatFactureModule, DetAchatLivraisonModule, DetailBonSortieModule, DetBulttinLivraisonModule, DetBulttinPaiModule, DetFactureModule, DetImportationsModule, DetLivraisonModule, FactureModule, ImportationsModule, LivraisonModule, PlanComptableModule, SoldeModule, PersonnelModule, VoitureModule, FourniseurModule, FournisseurModule],
+  imports: [VilleModule, EtablissementModule, ParamPrimeModule, ParamComissModule, RepertoireModule, StockModule, EmployeModule, DroitModule, DatabaseModule, AbsenceModule, AchatFactureModule, AchatLivraisonModule, BalanceModule, BilanModule, BonSortieModule, BulttinPaiModule, CaisseModule, ChequeModule, ComptaModule, ComptabiliteModule, CompteCaisseModule, DetAchatFactureModule, DetAchatLivraisonModule, DetailBonSortieModule, DetBulttinLivraisonModule, DetBulttinPaiModule, DetFactureModule, DetImportationsModule, DetLivraisonModule, FactureModule, ImportationsModule, LivraisonModule, PlanComptableModule, SoldeModule, PersonnelModule, VoitureModule, FournisseurModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/param-prime-produit/dto/update-param-prime-produit.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateParamPrimeProduitDto } from './create-param-prime-produit.dto';
-
-export class UpdateParamPrimeProduitDto extends PartialType(CreateParamPrimeProduitDto) {}
