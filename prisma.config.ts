e0d8caa592ff7eb09ee017add8991cc1dd98e250
abdellaho <bojaneabdellaho@gmail.com>
2025-11-19 15:43:44 +0100

    add another changes in schema by addin rc column in etablissement

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+/*import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from 'generated/prisma';
+
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "root",
+  database: "inventory_db",
+  connectionLimit: 5
+});
+const prisma = new PrismaClient({ adapter });*/
